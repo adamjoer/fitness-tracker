af97691f3d4a2b63722ddbd476f99f7b40b5094b
Adam Harald Jørgensen <58829763+adamjoer@users.noreply.github.com>
2023-05-16 22:30:43 +0200

    Add getter for user's full name in UserService

--- FitnessTracker/Data/UserService.cs
+++ FitnessTracker/Data/UserService.cs
@@ -27,4 +27,25 @@ public class UserService
             return await context.Users.FindAsync(id);
         });
     }
+
+    public Task<string?> GetUserFullName(string id)
+    {
+        return MemoryCache.GetOrCreateAsync($"GetUserFullName:{id}", async e =>
+        {
+            e.SetOptions(new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(5)
+            });
+            await using var context = await _dbContextFactory.CreateDbContextAsync();
+
+            var user = await context.Users.Where(user => user.Id == id)
+                .Select(user => new FitnessTrackerUser()
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                }).FirstOrDefaultAsync();
+
+            return user == null ? null : $"{user.FirstName} {user.LastName}";
+        });
+    }
 }
